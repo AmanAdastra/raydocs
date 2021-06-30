0917af3268b2bf08706ff86317b599b3bd16722d
unknown <amanayankhan78@gmail.com>
2021-06-30 17:33:02 +0530

    i added

--- raydocs/views.py
+++ raydocs/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import MyIdea
 # Create your views here.
+i=0
 def home(request):
+    global i
+    i+=1
     allidea = MyIdea.objects.all()
     if request.method=='POST':
         name = request.POST.get('instaid','unknown')
         idea = request.POST.get('idea','unknown')
         contri = MyIdea(name=name,idea=idea)
         contri.save()        
-    return render(request,'home.html',{'allidea':allidea})
+    return render(request,'home.html',{'allidea':allidea,'totalvisit':i})
