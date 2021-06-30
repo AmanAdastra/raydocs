4269a6271c84b0941925860735fce987a820a910
unknown <amanayankhan78@gmail.com>
2021-06-30 11:54:24 +0530

    db changed

--- core/settings.py
+++ core/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
 }
 
 import dj_database_url
-db_from-env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
