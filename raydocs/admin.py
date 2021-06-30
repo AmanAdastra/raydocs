1c38af9453f04e90c5a69316e3ea315c1987324b
unknown <amanayankhan78@gmail.com>
2021-06-30 16:31:33 +0530

    silly mistake

--- raydocs/admin.py
+++ raydocs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import MyIdea
 # Register your models here.
+@admin.register(MyIdea)
 class AdminIdea(admin.ModelAdmin):
     list_display = ['id','name','idea']
